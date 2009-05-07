8330cb3fadceda46f9c640c98d2b4a3f4b86e3d0
alexteam <alexteam@3d4681b2-48fc-5b48-9d53-c5846eff290b>
2009-05-07 09:59:21 +0000

    залил недостающий модуль. сменил dfm/dcr и прочим свойства на application/octet-stream
    git-svn-id: http://l2ph.coderx.ru/svn/l2phx3@118 3d4681b2-48fc-5b48-9d53-c5846eff290b

---
+++ 3.6.x/units/uLangSelectDialog.pas
@@ -0,0 +1,45 @@
+unit uLangSelectDialog;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, siLangCombo, siComp;
+
+type
+  TfLangSelectDialog = class(TForm)
+    siLang1: TsiLang;
+    siLangCombo1: TsiLangCombo;
+    Label2: TLabel;
+    Button1: TButton;
+    Button2: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fLangSelectDialog: TfLangSelectDialog;
+
+implementation
+uses uGlobalFuncs, umain;
+{$R *.dfm}
+
+procedure TfLangSelectDialog.Button1Click(Sender: TObject);
+begin
+close;
+if siLangCombo1.ItemIndex = -1 then exit;
+fMain.siLangDispatcher.Language := siLangCombo1.Items.Strings[siLangCombo1.ItemIndex];
+Options.WriteInteger('General', 'language', fLangSelectDialog.siLangCombo1.ItemIndex);
+Options.UpdateFile;
+end;
+
+procedure TfLangSelectDialog.Button2Click(Sender: TObject);
+begin
+close;
+end;
+
+end.
